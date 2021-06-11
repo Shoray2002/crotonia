1f65591249c6a9e81c72d736f78cdd4932e78095
Shoray Singhal <lci2020037@iiitl.ac.in>
2021-06-12 01:48:36 +0530

    themes added along with toggle

--- resources/js/script.js
+++ resources/js/script.js
@@ -36,3 +36,20 @@
       //seconds
     }, 0);
 })();
+
+
+
+// theme toggle
+const body=document.body;
+const toggle=document.getElementById("toggle")
+toggle.onclick=()=>{
+  if (body.classList.contains("light"))
+  {body.classList.remove("light")
+  body.classList.add("dark")}
+  else{
+    body.classList.remove("dark");
+    body.classList.add("light");
+  }
+  
+ }
+ 
